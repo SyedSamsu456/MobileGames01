a61fe6d8168f114ffa8568ca591bc2069bda0644
SyedSamsu <syedsamsu1111@gmail.com>
2022-01-03 16:05:17 +0530

    ss

--- src/test/java/com/capg/mobilegames/suite/MobileGamesApplicationTest.java
+++ src/test/java/com/capg/mobilegames/suite/MobileGamesApplicationTest.java
@@ -1,8 +1,9 @@
-package com.capg.mobilegames;
+package com.capg.mobilegames.suite;
 
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @Suite
 @SelectPackages({"com.capg.mobilegames.controller"
